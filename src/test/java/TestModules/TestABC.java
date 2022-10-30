505f813346e5379391a62b531fdfc9a39271e22c
simil4447git <sriyasimil@gmail.com>
2022-10-30 17:25:32 +0530

    Test

---
+++ src/test/java/TestModules/TestABC.java
@@ -0,0 +1,5 @@
+package TestModules;
+
+public class TestABC {
+
+}
--- src/test/java/TestModules/TestNG2.java
+++ src/test/java/TestModules/TestNG2.java
@@ -14,6 +14,9 @@ public class TestNG2 {
 	public void TestMethod2()
 	{
 		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("4");
 	}
 	
 	@Test
